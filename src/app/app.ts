aa3c5b85dbb765af1f7f6b240ba0f2b1dd757b48
John Detterline <nsdevinc@gmail.com>
2026-03-04 16:36:23 -0500

    Created basic page layout.  Alert bar will be addressed later.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
+import { StatusBar } from './pageLayout/status-bar/status-bar';
+import { TitleBar } from './pageLayout/title-bar/title-bar';
+import { NavBar } from './pageLayout/nav-bar/nav-bar';
+import { FooterBar } from './pageLayout/footer-bar/footer-bar';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,StatusBar,TitleBar,NavBar,FooterBar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/pageLayout/title-bar/title-bar.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-title-bar',
+  imports: [],
+  templateUrl: './title-bar.html',
+  styleUrl: './title-bar.scss',
+})
+export class TitleBar {}
